e31c8a3f90174e46dfc30acbed222de9b741f512
PradipMurmu <pradiputtama2117@gmail.com>
2024-01-18 01:40:29 +0530

    Comments are added.

--- sample_cpp_program.cpp
+++ sample_cpp_program.cpp
@@ -3,14 +3,18 @@ using namespace std;
 
 int main()
 {
+    // Declarations of variables
     float number1, number2, sum, average;
+    //take input of two number 
     cout <<"Enter two numbers: ";
     cin >> number1;
     cin >> number2;
     
+    // Perform the operations
     sum = number1 + number2;
     average = sum / 2;
     
+    // Display the output
     cout <<" Sum = " << sum << "\n";
     cout <<"Average = " << average << "\n";
     
