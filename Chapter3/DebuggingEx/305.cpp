4743065a5bf253fadca2b165a65b1c6bedbe634a
PRADIP KUMAR MURMU <46897789+peekay21@users.noreply.github.com>
2024-01-18 21:05:49 +0000

    There is an error 305.cpp. Debugged it.

--- Chapter3/DebuggingEx/305.cpp
+++ Chapter3/DebuggingEx/305.cpp
@@ -1,22 +1,32 @@
-#include<iostream>
-#include<malloc.h>
+#include <iostream>
+#include <cstring>
+
 using namespace std;
 
 char* allocateMemory();
-int main()
-{
+
+int main() {
     char* str;
     str = allocateMemory();
-    cout<<str;
-    delete str;
-    str = "     ";
     cout << str;
+
+    // Correct dynamic memory deallocation
+    delete[] str;
+
+    // Assigning a string literal to str is not allowed
+    // Instead, allocate new memory and copy the string
+    str = new char[6];
+    strcpy(str, "  Pradip   ");
+    cout << str;
+
+    // Correct dynamic memory deallocation
+    delete[] str;
+
     return 0;
 }
 
-char* allocateMemory()
-{
-
-    char str[] = "Memory allocation Test";
+char* allocateMemory() {
+    char* str = new char[21];  // Allocate memory for the string "Memory allocation Test"
+    strcpy(str, "Memory allocation Test");
     return str;
 }
